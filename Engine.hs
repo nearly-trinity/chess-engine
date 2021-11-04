b2b2320a8ee99e14f0d4f2f84cf936fd585a3dcb
Asjal Ahmad <aahmad@trinity.edu>
2021-11-04 12:26:21 -0500

    Styles Changes removes redundant brackets, uses ascii char functions

--- Engine.hs
+++ Engine.hs
@@ -3,6 +3,7 @@ import Data.List.Split
 import Data.Maybe
 import Data.Set (intersection, difference, singleton, Set, fromList, member, insert)
 import qualified Data.Set as Set
+import Data.Char (isAsciiLower, isAsciiUpper)
 
 type GameState = (Color, Board)
 
@@ -12,7 +13,7 @@ getBoard :: GameState -> Board
 getBoard (_,board) = board
 
 type Won = Bool
-data Turn = TColor Color deriving (Show, Eq)
+newtype Turn = TColor Color deriving (Show, Eq)
 type Board = [(Location, Piece)]
 data Piece = Piece { pColor :: Color,
                      pType :: PieceType }
@@ -35,7 +36,7 @@ instance Show Piece where
     show (Piece Black Rook) = "r"
     show (Piece Black Bishop) = "b"
     show (Piece Black Knight) = "n"
-    show (Piece Black Pawn) = "p"            
+    show (Piece Black Pawn) = "p"
 
 type RowNum = Int
 type ColNum = Int
@@ -45,23 +46,23 @@ readRow _ 9 _ = [] -- edge of the board
 readRow [] _ _= [] -- end of string
 readRow (char:str) rowNum colNum
     | char >= '1' && char <= '9' = -- blank spaces
-        (Nothing : readRow str rowNum (colNum + (read (char:[]) :: Int)))
-    | (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') = let -- black / white pieces
-        piece = case char of
-            'k' -> Piece Black King 
-            'q' -> Piece Black Queen 
-            'r' -> Piece Black Rook 
-            'b' -> Piece Black Bishop  
-            'n' -> Piece Black Knight 
-            'p' -> Piece Black Pawn  
-            'K' -> Piece White King  
-            'Q' -> Piece White Queen 
-            'R' -> Piece White Rook 
-            'B' -> Piece White Bishop 
-            'N' -> Piece White Knight 
-            'P' -> Piece White Pawn 
-            x -> error "invalid piece"
-        in (Just ((colNum, rowNum), piece) : readRow str rowNum (colNum+1))
+        Nothing : readRow str rowNum (colNum + (read [char] :: Int))
+    | isAsciiLower char || isAsciiUpper char = let -- black / white pieces
+    piece = case char of
+        'k' -> Piece Black King
+        'q' -> Piece Black Queen
+        'r' -> Piece Black Rook
+        'b' -> Piece Black Bishop
+        'n' -> Piece Black Knight
+        'p' -> Piece Black Pawn
+        'K' -> Piece White King
+        'Q' -> Piece White Queen
+        'R' -> Piece White Rook
+        'B' -> Piece White Bishop
+        'N' -> Piece White Knight
+        'P' -> Piece White Pawn
+        x -> error "invalid piece"
+    in (Just ((colNum, rowNum), piece) : readRow str rowNum (colNum+1))
     | otherwise = error "invalid parse input"
 
 -- "5r2/2p2rb1/1pNp4/p2Pp1pk/2P1K3/PP3PP1/5R2/5R2 | w | - | - | 1 | 51"
@@ -74,19 +75,19 @@ startingBoard = getBoard startingState
 readBoard :: String -> GameState
 readBoard input = let
     (boardData:rest) = splitOn " " input
-    turn = case (head rest) of -- this is either "w" or "b"
+    turn = case head rest of -- this is either "w" or "b"
         "w" -> White
         "b" -> Black
         x -> error "invalid turn"
-    board = case (head rest) of
+    board = case head rest of
         "w" -> let
-            rows = [8,7..1] `zip` (splitOn "/" boardData)
+            rows = [8,7..1] `zip` splitOn "/" boardData
             in concat [readRow str rowNum 1 | (rowNum, str) <- rows]
         "b" -> let
-            rows = [1..8] `zip` (splitOn "/" boardData)
+            rows = [1..8] `zip` splitOn "/" boardData
             in concat [readRow str rowNum 1 | (rowNum, str) <- rows]
         x -> error "invalid turn"
-    in (turn, (catMaybes board))
+    in (turn, catMaybes board)
 
 ----------------------------------------------------------------------------
 --                          Legal Moves
@@ -97,7 +98,7 @@ inBounds row col = row <= 8 && row >= 1 && col <= 8 && col >= 1
 
 lookupLoc :: Board -> Piece -> Maybe Location
 lookupLoc [] _ = Nothing
-lookupLoc ((loc,piece):pieces) query = 
+lookupLoc ((loc,piece):pieces) query =
     if piece == query
     then Just loc
     else lookupLoc pieces query
@@ -106,10 +107,10 @@ otherPieces :: Board -> Piece -> [(Location, Piece)]
 otherPieces board thisPiece = filter (\(loc, piece) -> piece /= thisPiece) board
 
 negDiagMoves :: (RowNum, ColNum) -> [(RowNum, ColNum)]
-negDiagMoves (x,y) = [(x+offset, y-offset) | offset <- [-7..7], inBounds (x+offset) (y-offset)] 
+negDiagMoves (x,y) = [(x+offset, y-offset) | offset <- [-7..7], inBounds (x+offset) (y-offset)]
 
 posDiagMoves :: (RowNum, ColNum) -> [(RowNum, ColNum)]
-posDiagMoves (x,y) = [(x+offset, y+offset) | offset <- [-7..7], inBounds (x+offset) (y+offset)] 
+posDiagMoves (x,y) = [(x+offset, y+offset) | offset <- [-7..7], inBounds (x+offset) (y+offset)]
 
 rowMoves :: (RowNum, ColNum) -> [(RowNum, ColNum)]
 rowMoves (x,y) = [(x+offset,y) | offset <- [-7..7], inBounds (x+offset) y]
@@ -125,12 +126,12 @@ colMoves (x,y) = [(x,y+offset) | offset <- [-7..7], inBounds x (y+offset)]
 testGetMoves = getMoves startingBoard (Piece Black Queen)
 
 
-getMoves :: Board -> Piece -> Set Location 
+getMoves :: Board -> Piece -> Set Location
 getMoves board piece =
-    case piece of 
+    case piece of
         Piece Black Queen -> let
             (x,y) = fromJust $ lookupLoc board piece
-            allMoves = fromList ((rowMoves (x,y)) ++ (colMoves (x,y)) ++ (posDiagMoves (x,y)) ++ (negDiagMoves (x,y)))
+            allMoves = fromList (rowMoves (x,y) ++ colMoves (x,y) ++ posDiagMoves (x,y) ++ negDiagMoves (x,y))
             notBlkQueen = fromList $ map fst $ otherPieces board piece
             collisions = allMoves `intersection` notBlkQueen
             -- need helper
@@ -147,7 +148,7 @@ getMoves board piece =
 
 
 
-        
+
 
 
 
